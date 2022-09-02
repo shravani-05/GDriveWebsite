e6a2af7a0fb8e48fb422b56e3b66c861a388cbdf
Shravani <sawantshravani03@gmail.com>
2022-09-02 17:04:46 +0530

    styling

--- script.js
+++ script.js
@@ -14,3 +14,29 @@ function validate()
           return false;
         }
       }
+
+
+      //*
+    //   <div class="title-header" style="display: flex; justify-content:space-between;" >
+    // <header style="display: inline; justify-content:space-between; padding: 1rem;">
+    
+    //         <h1>
+    //             <label for="">
+    //                 <span class="las la-bars"></span>
+    //             </label>
+    //             Dashboard
+    //         </h1>
+    //         <div class="search-wrapper">
+    //             <!-- <span class="las la-search"></span> -->
+    //             <input type="search" placeholder="Search here" id="">
+    //         </div>
+    //         <div class="user-wrapper">
+    //             <!-- <img src="vsw&l.jpg" width="100px" height="40px" alt=""> -->
+    //             <div>
+    //                 <h4> Shravani Sawant</h4>
+    //                 <small>Admin</small>
+
+    //             </div>
+
+    //     </div>
+    */
